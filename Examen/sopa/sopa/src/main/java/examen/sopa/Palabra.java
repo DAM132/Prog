c695723fd88a3779a2011d771df5cd57a4b1cf27
DAM132 <gines.priederivero@iesmiguelherrero.com>
2023-01-25 14:07:23 +0100

    subida

--- Examen/sopa/sopa/src/main/java/examen/sopa/Palabra.java
+++ Examen/sopa/sopa/src/main/java/examen/sopa/Palabra.java
@@ -9,5 +9,17 @@ package examen.sopa;
  * @author gines
  */
 public class Palabra {
-    
+	private String palabra;
+	private int posX;
+	private int posY;
+	private int direccion;
+	
+	public Palabra(String palabra, int posX, int posY) {
+		this.palabra = palabra;
+		this.posX = posX;
+		this.posY = posY;
+		this.direccion = direccion;
+	}
+	
+
 }
